e1f78fb913e4306573b8856093b16f02c7c20b34
OmitNomis <simon98190@gmail.com>
2023-10-16 11:37:59 +0545

    configure theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ThemeSwitcher } from "./components/ThemeSwitcher";
 import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
     <div className="bg-background text-foreground">
       <Button>Hello World</Button>
+      <ThemeSwitcher />
     </div>
   );
 };
