d95e55f5813c9b541e98976649acb28b88fd6367
OmitNomis <simon98190@gmail.com>
2023-10-16 11:33:16 +0545

    config shadcn for theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div className="bg-background text-foreground">
+      <Button>Hello World</Button>
+    </div>
+  );
 };
 export default App;
