3a7480bbf0d2c4d0d4bd12613971b37736996967
OmitNomis <simon98190@gmail.com>
2023-10-17 18:10:03 +0545

    config useToast

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { AppSection } from "./components/Main";
+import { Toaster } from "./components/ui/toaster";
 const App = () => {
   return (
     <div className="bg-background text-foreground min-h-screen flex flex-col items-center">
+      <Toaster />
       <Header />
       <AppSection />
       <Footer />
