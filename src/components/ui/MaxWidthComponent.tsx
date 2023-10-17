719d09945e143bf1571aa0c3dc324a56efc7d091
OmitNomis <simon98190@gmail.com>
2023-10-17 10:47:00 +0545

    container changes

--- src/components/ui/MaxWidthComponent.tsx
+++ src/components/ui/MaxWidthComponent.tsx
@@ -1,9 +1,27 @@
+type Tag = "div" | "section" | "article" | "main" | "header" | "footer";
+
 export const MaxWidthComponent = ({
   children,
   className,
+  hasPadding = true,
+  as = "div",
 }: {
   children: React.ReactNode;
   className?: string;
+  hasPadding?: boolean;
+  as?: Tag;
 }) => {
-  return <div className={`w-full max-w-5xl ${className}`}>{children}</div>;
+  const getClassName = () => {
+    let newClassName = `w-full max-w-4xl`;
+    if (hasPadding) {
+      newClassName += ` p-2 sm:p-4`;
+    }
+    if (className) {
+      newClassName += ` ${className}`;
+    }
+    return newClassName;
+  };
+
+  const Tag = as;
+  return <Tag className={`${getClassName()}`}>{children}</Tag>;
 };
