34fecad2eb929b1be8d4a3cd8fc9144ad11fc142
OmitNomis <simon98190@gmail.com>
2023-10-18 14:16:06 +0545

    config looping player

--- src/components/AppSection.tsx
+++ src/components/AppSection.tsx
@@ -5,14 +5,25 @@ import { MaxWidthComponent } from "./ui/MaxWidthComponent";
 import { useState } from "react";
 
 export const AppSection = () => {
-  const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
+  const [isPlayerPlaying, setIsPlayerPlaying] = useState(false);
+  const [timeRange, setTImeRange] = useState([0, 0]);
+
+  const handleURLEntered = (url: string) => {
+    setSearch(url);
+    setIsPlayerPlaying(true);
+  };
 
   return (
     <MaxWidthComponent as="main" className="flex flex-grow flex-col">
-      <URLInput setSearch={setSearch} />
+      <URLInput setSearch={handleURLEntered} />
       <PlayerWrapper>
-        <LoopingPlayer url={search} />
+        <LoopingPlayer
+          url={search}
+          startTime={0}
+          endTime={10}
+          isPlaying={isPlayerPlaying}
+        />
       </PlayerWrapper>
     </MaxWidthComponent>
   );
--- src/components/LoopingPlayer.tsx
+++ src/components/LoopingPlayer.tsx
@@ -1,13 +1,38 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import ReactPlayer from "react-player";
+import { OnProgressProps } from "react-player/base";
 
 interface LoopingPlayerProps {
   url: string;
+  startTime: number;
+  endTime: number;
+  isPlaying: boolean;
 }
 
-export const LoopingPlayer: FC<LoopingPlayerProps> = ({ url }) => {
+export const LoopingPlayer: FC<LoopingPlayerProps> = ({
+  url,
+  isPlaying,
+  startTime,
+  endTime,
+}) => {
+  const playerRef = useRef<ReactPlayer>(null);
+  const handleProgress = (progress: OnProgressProps) => {
+    if (progress.playedSeconds >= endTime) {
+      playerRef.current?.seekTo(startTime);
+    }
+  };
+
   if (url) {
-    return <ReactPlayer url={url} playing={true} loop={true} controls={true} />;
+    return (
+      <ReactPlayer
+        ref={playerRef}
+        url={url}
+        playing={isPlaying}
+        loop={true}
+        controls={true}
+        onProgress={handleProgress}
+      />
+    );
   }
 
   return (
